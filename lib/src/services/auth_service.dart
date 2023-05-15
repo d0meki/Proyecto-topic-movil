8d193d339fe1cd15088947f259944a79f64ded6a
d0meki <domeki151190@gmail.com>
2023-05-15 17:18:39 -0400

    solo falta el node

--- lib/src/services/auth_service.dart
+++ lib/src/services/auth_service.dart
@@ -1,32 +1,68 @@
 import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-class AuthService {
+
+class AuthServices {
   final baseUrl = 'http://34.176.0.165:3000/api';
   var dio = Dio();
-
-  AuthService();
-
-  login() {
-    //retornar user bloqueado
-  }
-  registrar() {
-    //reotornar correo existente
-  }
-
- Future verificarCi(String ci) async {
-    final url = Uri.parse('$baseUrl/auth/verificar-ci');
+  AuthServices();
+  Future<dynamic> login(email, password) async {
+    final url = Uri.parse('$baseUrl/auth/login-user');
     try {
       final res = await http.post(url,
-          headers: {'Accept': 'application/json'}, body: {"ci":ci});
+          headers: {'Accept': 'application/json'}, body: {email, password});
       final data = jsonDecode(res.body);
-      print(data);
-      // return data;
+      return data;
     } catch (e) {
-      print('Sucedio algun error');
-      print(e);
+      return e;
     }
   }
 
-  verificarFotoSegip() {}
+  Future<dynamic> registrarUsuario(
+      Map<String, dynamic> usuario, imagenPath, fileName) async {
+    final url = Uri.parse('$baseUrl/auth/register-user');
+    try {
+      final res = await http.post(url,
+          headers: {'Accept': 'application/json'},
+          body: {usuario, imagenPath, fileName});
+      final data = jsonDecode(res.body);
+      return data;
+    } catch (e) {
+      return e;
+    }
+  }
+  Future<dynamic> verificarCi(String ci) async {
+    final url = Uri.parse('$baseUrl/auth/verificar-ci');
+    try {
+      final res = await http
+          .post(url, headers: {'Accept': 'application/json'}, body: {"ci": ci});
+      final data = jsonDecode(res.body);
+      return data;
+    } catch (e) {
+      return e;
+    }
+  }
+  Future<bool> verificarFoto(String image64, String uuid) async {
+    final url = Uri.parse('$baseUrl/auth/verificar-foto');
+    try {
+      final res = await http.post(url,
+          headers: {'Accept': 'application/json'}, body: {image64, uuid});
+      final data = jsonDecode(res.body);
+      return data;
+    } catch (e) {
+      return false;
+    }
+  }
+  Future<dynamic> changePassword(String newPassword) async {}
+  Future<bool> signOut(user) async {
+    final url = Uri.parse('$baseUrl/auth/logout');
+    try {
+      final res = await http.post(url,
+          headers: {'Accept': 'application/json'}, body: {user});
+      final data = jsonDecode(res.body);
+      return data;
+    } catch (e) {
+      return false;
+    }
+  }
 }
